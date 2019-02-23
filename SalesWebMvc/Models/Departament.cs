40bccd944ee100b2a08173820640dfd61b959300
Luan <luanlazzari@hotmail.com>
2019-02-23 17:39:57 -0300

    First Model-View_Controler - Departament

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Departament()
+        {
+        }
+    }
+}
